5b5221c73ececf51a6fe0c41452209e98d5a1a6c
Smartech <rain2540@foxmail.com>
2016-04-13 16:50:29 +0800

    Add sub class of UIScreen which called MainScreen

---
+++ RGUICategory/RGUICategory/MainScreen.h
@@ -0,0 +1,23 @@
+//
+//  MainScreen.h
+//  RGUICategory
+//
+//  Created by RAIN on 16/4/13.
+//  Copyright © 2016年 Smartech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainScreen : UIScreen
+
++ (CGRect)rg_bouds;
+
++ (CGSize)rg_size;
+
++ (CGFloat)rg_width;
+
++ (CGFloat)rg_height;
+
++ (UIScreen *)rg_mainScreen;
+
+@end
